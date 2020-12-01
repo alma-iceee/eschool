0c2b54da74c00eafae5918bb4403d3f955036a34
alma-iceee <alma.iceee@gmail.com>
2020-12-01 16:50:02 +0600

    Adding lombok @Data annotation to Student class

--- src/main/java/kz/greetgo/eschool/Student.java
+++ src/main/java/kz/greetgo/eschool/Student.java
@@ -1,52 +1,13 @@
 package kz.greetgo.eschool;
 
+import lombok.Data;
+
+@Data
 public class Student {
 
     private Long id;
     private String name;
     private String surname;
     private String course;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public String getCourse() {
-        return course;
-    }
-
-    public void setCourse(String course) {
-        this.course = course;
-    }
-
-    @Override
-    public String toString() {
-        return "Student{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", surname='" + surname + '\'' +
-                ", course='" + course + '\'' +
-                '}';
-    }
 
 }
