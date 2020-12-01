9a7a6a30d8429184c1609efeb627b3e83daa5374
alma-iceee <alma.iceee@gmail.com>
2020-12-01 16:29:56 +0600

    Testing Student mapper

--- src/main/java/kz/greetgo/eschool/EschoolApplication.java
+++ src/main/java/kz/greetgo/eschool/EschoolApplication.java
@@ -1,7 +1,9 @@
 package kz.greetgo.eschool;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class EschoolApplication {
@@ -10,4 +12,21 @@ public class EschoolApplication {
         SpringApplication.run(EschoolApplication.class, args);
     }
 
+    private final StudentMapper studentMapper;
+
+    public EschoolApplication(StudentMapper studentMapper) {
+        this.studentMapper = studentMapper;
+    }
+
+    @Bean
+    CommandLineRunner sampleCommandLineRunner() {
+        return args -> {
+            Student student = new Student();
+            student.setName("Almas");
+            student.setSurname("Sharipkhan");
+            student.setCourse("CSSE-1603");
+            studentMapper.insert(student);
+            System.out.println(this.studentMapper.findAll());
+        };
+    }
 }
