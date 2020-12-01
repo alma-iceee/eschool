5c8368b4d98c675166eb43cc7723d8b7f0fbf0d6
alma-iceee <alma.iceee@gmail.com>
2020-12-01 16:47:13 +0600

    Creating Student controller

---
+++ src/main/java/kz/greetgo/eschool/StudentController.java
@@ -0,0 +1,20 @@
+package kz.greetgo.eschool;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class StudentController {
+
+    @Autowired
+    private StudentService studentService;
+
+    @GetMapping("/students")
+    public List<Student> student() {
+        return studentService.findAll();
+    }
+
+}
